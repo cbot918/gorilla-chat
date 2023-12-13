509eb50d92fbc574caccd7d8f7197ff17b39bc33
cbot918 <cbot918@gmail.com>
2023-12-13 16:15:08 +0800

    impl offline users

--- ui/src/components/friend/Friend.jsx
+++ ui/src/components/friend/Friend.jsx
@@ -3,9 +3,9 @@ import M from 'materialize-css'
 function Friend(){
   const [email, setEmail] = useState("")
   const [allUsers, setAllUsers] = useState({})
-  // const [offlineUsers, setOfflineUsers] = useState({})
+  const [offlineUsers, setOfflineUsers] = useState({})
   const [onlineUsers, setOnlineUsers] = useState({})
-  // const [isLoadingOffline, setIsLoadingOffline] = useState(true); // Loading state
+  const [isLoadingOffline, setIsLoadingOffline] = useState(true); // Loading state
   const [isLoadingOnline, setIsLoadingOnline] = useState(true); // Loading state
   const [isLoadingAll, setIsLoadingAll] = useState(true); // Loading state
 
@@ -39,41 +39,53 @@ function Friend(){
 
 
   function getOnlineUsers(token){
-    fetch("http://localhost:8088/user/online",{
+    return fetch("http://localhost:8088/user/online",{
       method:"get",
       headers:{
         "Content-Type": "application/json",
         "Authorization": token
       }
     })
-    .then(res => res.json())
-    .then(data => {
-      setOnlineUsers(data)
-      setIsLoadingOnline(false)
-    })
-    .catch(err=>{
-      console.log(err)
-      setIsLoadingOnline(false)
-    })
+    .then(res => {
+      if (!res.ok) {
+          throw new Error('request online users failed');
+      }
+      return res.json();
+    });
+    // .then(res => res.json())
+    // .then(data => {
+    //   setOnlineUsers(data)
+    //   setIsLoadingOnline(false)
+    // })
+    // .catch(err=>{
+    //   console.log(err)
+    //   setIsLoadingOnline(false)
+    // })
   }
 
   function getAllUsers(token){
-    fetch("http://localhost:8088/user/all",{
+    return fetch("http://localhost:8088/user/all",{
       method:"get",
       headers:{
         "Content-Type": "application/json",
         "Authorization": token
       }
     })
-    .then(res => res.json())
-    .then(data => {
-      setAllUsers(data)
-      setIsLoadingAll(false)
-    })
-    .catch(err=>{
-      console.log(err)
-      setIsLoadingAll(false)
-    })
+    .then(res => {
+      if (!res.ok) {
+          throw new Error('request all users failed');
+      }
+      return res.json();
+    });
+    // .then(res => res.json())
+    // .then(data => {
+    //   setAllUsers(data)
+    //   setIsLoadingAll(false)
+    // })
+    // .catch(err=>{
+    //   console.log(err)
+    //   setIsLoadingAll(false)
+    // })
   }
 
   // function getOfflineUsers(token){
@@ -101,14 +113,23 @@ function Friend(){
     Promise.all([getOnlineUsers(token), getAllUsers(token)])
       .then(([onlineUsersData, allUsersData]) => {
         setOnlineUsers(onlineUsersData);
+        setIsLoadingOnline(false)
+
         setAllUsers(allUsersData);
+        setIsLoadingAll(false)
 
         console.log(onlineUsersData)
+        console.log(allUsersData)
 
-        // const offlineUsers = allUsersData.filter(user => 
-        //   !onlineUsersData.some(onlineUser => onlineUser.id === user.id)
+        let offlines = allUsersData.names.filter(x => !onlineUsersData.users.includes(x));
+        console.log(offlines)
+        setOfflineUsers(offlines)
+        setIsLoadingOffline(false)
+        
+        // const offlineUsers = allUsersData.names.filter(name => 
+        //   !onlineUsersData.some(onlineUser.users => onlineUser.id === user.id)
         // );
-        // // Now you have offline users separated
+
         // console.log("Offline Users:", offlineUsers);
       })
       .catch(error => {
@@ -133,7 +154,7 @@ function Friend(){
             <span>({ onlineUsers.count -1 }) online users: [ </span>{ !isLoadingOnline && onlineUsers ?<RenderOnlineUsers onlineUsers={onlineUsers}/>:<span>is loading...</span>}<span>]</span>
           </div>
 
-          {/* <input 
+          <input 
             type="button" 
             value="刷新offline" 
             onClick={()=>{
@@ -141,9 +162,9 @@ function Friend(){
             }}
           />
           <div>
-            <span>({ offlineUsers.count -1 }) offline users: [ </span>{ !isLoadingOffline && offlineUsers ?<RenderOfflineUsers offlineUsers={offlineUsers}/>:<span>is loading...</span>}<span>]</span>
-          </div> */}
-          <input 
+            <span>({ offlineUsers.length }) offline users: [ </span>{ !isLoadingOffline && offlineUsers ?<RenderOfflineUsers offlineUsers={offlineUsers}/>:<span>is loading...</span>}<span>]</span>
+          </div>
+          {/* <input 
             type="button" 
             value="刷新all" 
             onClick={()=>{
@@ -151,8 +172,8 @@ function Friend(){
             }}
           />
           <div>
-            <span>({ allUsers.count -1 }) offline users: [ </span>{ !isLoadingAll && allUsers ?<RenderAllUsers allUsers={allUsers}/>:<span>is loading...</span>}<span>]</span>
-          </div>
+            <span>({ allUsers.count -1 }) all users: [ </span>{ !isLoadingAll && allUsers ?<RenderAllUsers allUsers={allUsers}/>:<span>is loading...</span>}<span>]</span>
+          </div> */}
         </div>
       {/* 好友申請的 code, 暫時留一下 */}
       {/* <input 
@@ -198,18 +219,16 @@ function RenderOnlineUsers({onlineUsers}){
   )
 }
 
-// function RenderOfflineUsers({offlineUsers}){
-//   return (
-//     <>
-//       {offlineUsers.users
-//         .filter((user)=>user != JSON.parse(localStorage.getItem('user')).name)
-//         .map((name, index)=>(
-//         <span key={index}>{name} </span>
-//       ))}
-//     </>
-    
-//   )
-// }
+function RenderOfflineUsers({offlineUsers}){
+  return (
+    <>
+      {offlineUsers
+        .map((name, index)=>(
+        <span key={index}>{name} </span>
+      ))}
+    </>
+  )
+}
 
 function RenderAllUsers({allUsers}){
   return (
